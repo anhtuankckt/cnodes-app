76277187b4253fa096028de16270194df1abe25c
anhtuankckt <anhtuankckt@gmail.com>
2024-05-24 12:31:23 +0700

    feate: client: build them login

---
+++ client/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      // Colors used in the project
+      colors: {
+        primary: '#2b85ff',
+        secondary: '2ef863e'
+      }
+    },
+  },
+  plugins: [],
+}
+
